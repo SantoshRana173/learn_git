410d02217756e99791573ba544b6535772c81059
Santosh Rana <ranasantosh173@gmail.com>
2024-08-12 21:40:45 +0900

    Add python code to the file

--- file.py
+++ file.py
@@ -1,1 +1,5 @@
-Hello
+#counting the substring usa ignoring case
+str1 = "Welcome, to USA. usa awesome, isn't it? Do you like usa?"
+sub_str = "usa"
+count = str1.lower().count(sub_str)
+print(count)
--- file1.py
+++ file1.py
@@ -1,1 +1,15 @@
+#Listing all the digits present in a string
+import re
+def my_func(data):
+    data_list = re.findall('[0-9]+', data)
+    return data_list
+
+def main():
+    str1 = "My favourite numbers are 19, 29, 8, and 1"
+    print(my_func(str1))
+
+
+if __name__ ==  '__main__': 
+    main()
+
 
